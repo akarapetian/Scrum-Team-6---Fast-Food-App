9a9541e7535dffcf93acf1f3adc965b81265045a
Anthony Karapetian <42709892+antboyo@users.noreply.github.com>
2019-03-02 06:02:01 -0800

    Began Taking Trips Functionality
    Added the interface to create a custom trip, and populate a queue with the restaurants to visit.
    Fixed some bugs with editing menus

--- Quick_Eats/mainwindow.h
+++ Quick_Eats/mainwindow.h
@@ -3,6 +3,7 @@
 
 #include <QMainWindow>
 #include <QVector>
+#include <QQueue>
 #include <QListWidget>
 #include "restaurant.h"
 
@@ -19,45 +20,53 @@ public:
     ~MainWindow();
 
 private slots:
+
+    //LOGIN FUNCTIONS
     void on_mainLoginButton_clicked();
-
     void on_loginButton_clicked();
-
-    void on_MWManageRestaurantsButton_clicked();
-
     void on_actionLogout_triggered();
 
+    //MANAGE RESTAURANTS FUNCTIONS
+    void on_MWManageRestaurantsButton_clicked();
     void on_MWManageBackButton_clicked();
-
-    void on_restaurantListWidget_currentItemChanged(QListWidgetItem *current, QListWidgetItem *previous);
-
+    void on_manageRestaurantListWidget_currentItemChanged(QListWidgetItem *current, QListWidgetItem *previous);
     void on_deleteRestaurantButton_clicked();
-
-    void on_restaurantListWidget_itemDoubleClicked(QListWidgetItem *item);
-
+    void on_manageRestaurantListWidget_itemDoubleClicked(QListWidgetItem *item);
     void on_addButton_clicked();
-
     void on_addRestaurantButton_clicked();
-
     void on_addItemButton_clicked();
-
-    void on_menuListWidget_currentItemChanged(QListWidgetItem *current, QListWidgetItem *previous);
-
-    void on_menuPriceListWidget_currentItemChanged(QListWidgetItem *current, QListWidgetItem *previous);
-
+    void on_manageMenuListWidget_currentItemChanged(QListWidgetItem *current, QListWidgetItem *previous);
+    void on_manageMenuPriceListWidget_currentItemChanged(QListWidgetItem *current, QListWidgetItem *previous);
     void on_addMenuItemButton_clicked();
-
-    void on_menuListWidget_itemDoubleClicked(QListWidgetItem *item);
-
-    void on_menuPriceListWidget_itemDoubleClicked(QListWidgetItem *item);
-
+    void on_manageMenuListWidget_itemDoubleClicked(QListWidgetItem *item);
+    void on_manageMenuPriceListWidget_itemDoubleClicked(QListWidgetItem *item);
     void on_deleteItemButton_clicked();
+
+
+
+    void on_customPathButton_clicked();
+
+    void on_shortestPathButton_clicked();
+
+
+    void on_MWTakeTripButton_clicked();
+
+    void on_customSelectRestaurantListWidget_itemChanged(QListWidgetItem *item);
+
+    void on_customTakeTripButton_clicked();
 
 private:
     Ui::MainWindow *ui;
 
     //declare vector of restaurants
     QVector<restaurant> restaurantsVector;
+
+    //queue that represents the trip the user is currently taking
+    QQueue<restaurant> currentTrip;
+
+    //this data structure holds all the custom trips that belong to a user
+    //it is a vector of pointers, each pointing to a Queue of restaurants (the custom trip).
+    QVector<QQueue<restaurant>*> customTrips;
 };
 
 #endif // MAINWINDOW_H
